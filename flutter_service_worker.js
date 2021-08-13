5be8bf479cc1f8167e2f25da075c1bb7f1a9cd24
LeoRc01 <45425606+LeoRc01@users.noreply.github.com>
2021-08-13 15:48:47 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "bd7f42c9873259002cc1a275ccaa4c8f",
-"index.html": "4f48517fd6b5230b7a75f9996780a893",
-"/": "4f48517fd6b5230b7a75f9996780a893",
-"main.dart.js": "fe755fe37db9289375dc71a39b1df9bf",
+"index.html": "e7fe025d7022351335f874f1a13b28d1",
+"/": "e7fe025d7022351335f874f1a13b28d1",
+"main.dart.js": "642126ee1b132ef2af1cd4661f478ed1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
